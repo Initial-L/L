e1a3491cc3e5c5aebecf963ec5d0d61dd287095d
L <sonzai-x@datacentral.my.id>
2023-02-16 17:52:52 +0700

    L

---
+++ update/about.sh
@@ -0,0 +1,14 @@
+ echo -e "
+>>>>>>>>>>Welcome to AutoScript Premium<<<<<<<<<<
+>Support OS 
+For Ubuntu 18 & Ubuntu 20 
+For Debian 9 & Debian 10
+
+Contact Owner AutoScript
+> TELEGRAM: t.me/Bionovember2k"
+
+echo -e "Thanks for your Order Arz AutoScript"
+echo ""
+read -n 1 -s -r -p "  Press any key to Back to Menu"
+menu
+fi
---
+++ update/menu-bandwith.sh
@@ -0,0 +1,265 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+colornow=$(cat /etc/arzvpn/theme/color.conf)
+NC="\e[0m"
+RED="\033[0;31m" 
+COLOR1="$(cat /etc/arzvpn/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/arzvpn/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"                    
+###########- END COLOR CODE -##########
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/Initial-L/WhoIsL/main/Kira > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+Name=$(curl -sS https://raw.githubusercontent.com/Initial-L/WhoIsL/main/Kira | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/Initial-L/WhoIsL/main/Kira | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
+clear
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$COLOR1      • BANDWITH MONITOR •         ${NC}"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "" 
+echo -e " [\e[36m•1\e[0m] Lihat Total Bandwith Tersisa"
+echo -e " [\e[36m•2\e[0m] Tabel Penggunaan Setiap 5 Menit"
+echo -e " [\e[36m•3\e[0m] Tabel Penggunaan Setiap Jam"
+echo -e " [\e[36m•4\e[0m] Tabel Penggunaan Setiap Hari"
+echo -e " [\e[36m•5\e[0m] Tabel Penggunaan Setiap Bulan"
+echo -e " [\e[36m•6\e[0m] Tabel Penggunaan Setiap Tahun"
+echo -e " [\e[36m•7\e[0m] Tabel Penggunaan Tertinggi"
+echo -e " [\e[36m•8\e[0m] Statistik Penggunaan Setiap Jam"
+echo -e " [\e[36m•9\e[0m] Lihat Penggunaan Aktif Saat Ini"
+echo -e " [\e[36m10\e[0m] Lihat Trafik Penggunaan Aktif Saat Ini [5s]"
+echo -e "" 
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e "" 
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1)
+clear 
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$COLOR1      • TOTAL BANDWITH SERVER •    ${NC}"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+
+vnstat
+
+echo -e ""
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-set
+;;
+
+2)
+clear 
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$COLOR1 • TOTAL BANDWITH SETIAP 5 MENIT • ${NC}"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+
+vnstat -5
+
+echo -e ""
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-set
+;;
+
+3)
+clear 
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$COLOR1   • TOTAL BANDWITH SETIAP JAM •   ${NC}"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+
+vnstat -h
+
+echo -e ""
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-set
+;;
+
+4)
+clear 
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$COLOR1  • TOTAL BANDWITH SETIAP HARI •   ${NC}"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+
+vnstat -d
+
+echo -e ""
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-set
+;;
+
+5)
+clear 
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$COLOR1  • TOTAL BANDWITH SETIAP BULAN •  ${NC}"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+
+vnstat -m
+
+echo -e ""
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-set
+;;
+
+6)
+clear 
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$COLOR1  • TOTAL BANDWITH SETIAP TAHUN •  ${NC}"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+
+vnstat -y
+
+echo -e ""
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-set
+;;
+
+7)
+clear 
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$COLOR1    • TOTAL BANDWITH TERTINGGI •   ${NC}"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+
+vnstat -t
+
+echo -e ""
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-set
+;;
+
+8)
+clear 
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$COLOR1 • STATISTIK TERPAKAI SETIAP JAM • ${NC}"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+
+vnstat -hg
+
+echo -e ""
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-set
+;;
+
+9)
+clear 
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$COLOR1     • LIVE BANDWITH SAAT INI •    ${NC}"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e   " Press [ Ctrl+C ] • To-Exit"
+echo -e ""
+
+vnstat -l
+
+echo -e ""
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-set
+;;
+
+10)
+clear 
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "$COLOR1• LIVE TRAFIK PENGGUNAAN BANDWITH •${NC}"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+
+vnstat -tr
+
+echo -e ""
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-set
+;;
+
+0)
+sleep 1
+menu
+;;
+*)
+echo -e ""
+echo -e "Anda salah tekan"
+sleep 1
+menu-set
+;;
+esac
